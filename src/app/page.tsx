280ebe6267615c13b95c98e5f8abef00ee73b36d
Cuistre <gaultier.hym@gmail.com>
2025-03-25 12:30:28 +0100

    clean 22/n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
       <div className="pt-16">
         <FadeInBlur delay={0}>
           <h1 className="text-4xl font-bold text-center mb-12">
-            Bienvenue sur mon blog
+            Bienvenue sur mon blog 22
           </h1>
         </FadeInBlur>
         <div className="max-w-5xl my-24 mx-auto px-4 flex flex-col md:flex-row gap-8">
